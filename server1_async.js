cf78e85cd1eccc0ffe67ffddd074060d649e0643
jamarpa <jamarpa6@inf.upv.es>
2015-10-04 17:44:37 +0200

    añadido servidor asíncrono de fichero específico

--- server1_async.js
+++ server1_async.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req,res){
-	var ret = fs.readFile('./test.txt',function(err,ret){
+	var ret = fs.readFile('.'+req.url,function(err,ret){
 	if (err) console.log("Error");
 	else {
 		res.writeHead(200,{'Content-Type':'text/plain'});
