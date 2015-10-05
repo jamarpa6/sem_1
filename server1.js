32bf328fc2d4cfaed8849c7b4e8ecbcf3c3fcb35
jamarpa <jamarpa6@inf.upv.es>
2015-10-05 09:12:42 +0200

    version con ficehro index

--- server1.js
+++ server1.js
@@ -2,9 +2,18 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req,res){
-	var ret = fs.readFileSync('.'+req.url);
-	res.writeHead(200,{'Content-Type':'text/plain'});
-	console.log("Estoy aqui");
-	console.log(ret);
-	res.end(ret);
+	if(req.url=='/' || req.url=='/index.html'){
+		var ret = fs.readFileSync('./public/index.html');
+		res.writeHead(200,{'Content-Type':'text/html'});
+		console.log("Estoy aqui");
+		console.log(ret);
+		res.end(ret);
+	}
+	else{
+		var ret = fs.readFileSync('.'+req.url);
+		res.writeHead(200,{'Content-Type':'text/plain'});
+		console.log("Estoy aqui");
+		console.log(ret);
+		res.end(ret);
+	}
 }).listen(9000);
