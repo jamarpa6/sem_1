1a97a46bbf7e2dee25754750ee62f410ecbe4b00
jamarpa <jamarpa6@inf.upv.es>
2015-10-05 09:22:00 +0200

    version con fichero index

--- server1_async.js
+++ server1_async.js
@@ -2,14 +2,34 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req,res){
-	var ret = fs.readFile('.'+req.url,function(err,ret){
-	if (err) console.log("Error");
-	else {
-		res.writeHead(200,{'Content-Type':'text/plain'});
-		console.log("Estoy aqui");
-		console.log(ret);
-		res.end(ret);
+	if(req.url=='/' || req.url=='/index.html'){
+		var ret = fs.readFile('./public/index.html',function (err,ret){
+	
+		if (err) console.log("Error");
+
+		else {
+			res.writeHead(200,{'Content-Type':'text/html'});
+			console.log("Estoy aqui");
+			console.log(ret);
+			res.end(ret);
+		}
+		});
 	}
-});
+	else{
+		var ret = fs.readFile('.'+req.url,function (err,ret){
+	
+		if (err) console.log("Error");
+
+		else {
+			res.writeHead(200,{'Content-Type':'text/plain'});
+			console.log("Estoy aqui");
+			console.log(ret);
+			res.end(ret);
+		}
+		}	
+	
+		);
+	}
+	
 	
 }).listen(9000);
