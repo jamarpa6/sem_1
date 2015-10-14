8e773e37c031ffc63ceb85f17fc4bfe50ce9dde6
jamarpa <jamarpa6@inf.upv.es>
2015-10-14 20:46:18 +0200

    servidor asincrono con errores tratados

--- server1.js
+++ server1.js
@@ -3,17 +3,41 @@ var fs = require('fs');
 
 http.createServer(function(req,res){
 	if(req.url=='/' || req.url=='/index.html'){
-		var ret = fs.readFileSync('./public/index.html');
-		res.writeHead(200,{'Content-Type':'text/html'});
-		console.log("Estoy aqui");
-		console.log(ret);
-		res.end(ret);
+		var ret;
+		try{
+			ret = fs.readFileSync('./public/index.html');
+		} catch(e){
+			res.writeHead(404,{'Content-Type':'text/html'});
+			res.end("<html><h1>Error 404 File not found</h1></html>");
+			console.log("Error capturado");
+		}
+		try{
+			res.writeHead(200,{'Content-Type':'text/html'});
+			console.log("Estoy aqui");
+			console.log(ret);
+			res.end(ret);
+		} catch(e){
+			res.writeHead(500,{'Content-Type':'text/html'});
+			res.end("<html><h1>Error 500 Internal server error</h1></html>");		
+		}
 	}
-	else{
-		var ret = fs.readFileSync('.'+req.url);
-		res.writeHead(200,{'Content-Type':'text/plain'});
-		console.log("Estoy aqui");
-		console.log(ret);
-		res.end(ret);
+
+	else{	var ret;
+		try{
+		 	ret = fs.readFileSync('.'+req.url);
+		} catch(e){
+			res.writeHead(404,{'Content-Type':'text/html'});
+			res.end("<html><h1>Error 404 File not found</h1></html>");
+		}
+		try{
+			res.writeHead(200,{'Content-Type':'text/plain'});
+			console.log("Estoy aqui");
+			console.log(ret);
+			res.end(ret);
+		} catch(e){
+			res.writeHead(500,{'Content-Type':'text/html'});
+			res.end("<html><h1>Error 500 Internal server error</h1></html>");
+
+		}
 	}
 }).listen(9000);
